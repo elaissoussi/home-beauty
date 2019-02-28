05154466d9a3417a3daee7c668af20377c7ebf43
Monsif EL AISSOUSSI <monsif.elaissoussi@external.chanel-corp.com>
2019-02-28 17:32:49 +0100

    fix Cors for Rest Repository

--- back/src/main/java/com/elaissoussi/back/BackApplication.java
+++ back/src/main/java/com/elaissoussi/back/BackApplication.java
@@ -3,6 +3,8 @@ package com.elaissoussi.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,10 +17,11 @@ public class BackApplication {
 	}
 	
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+	public WebMvcConfigurer configureCorsForWebMvc() {
 	  
 	  return new WebMvcConfigurerAdapter() {
-	      @Override
+	      
+	     @Override
 	      public void addCorsMappings(CorsRegistry registry) {
 	        registry
 	        .addMapping("/**")
@@ -28,5 +31,22 @@ public class BackApplication {
 	        .allowCredentials(false);
 	      }
 	  };
+	}
+	
+	@Bean
+	public RepositoryRestConfigurerAdapter configureCorsForRestRepository() {
+	  return new RepositoryRestConfigurerAdapter() {
+	   
+	    @Override
+	    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+	        config.getCorsRegistry()
+	        .addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowCredentials(false);
+	    }
+	  }
+	  ;
 	}
 }
