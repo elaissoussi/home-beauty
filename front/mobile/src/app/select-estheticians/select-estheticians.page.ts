7bbc409db0bb2b29ddfaa889a2319e77c292d356
elkhdim <abdelilahelkhdim1@gmail.com>
2019-11-16 22:53:57 +0100

    Filter liste esth

--- front/mobile/src/app/select-estheticians/select-estheticians.page.ts
+++ front/mobile/src/app/select-estheticians/select-estheticians.page.ts
@@ -4,9 +4,11 @@ import { AppointementService } from '../services/appointement.service';
 
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { Platform, ToastController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 import { EstheticianList } from '../models/EstheticianList';
 import { JsonConvert, Any } from "json2typescript";
+
+
 //import { IonicSelectableComponent } from 'ionic-selectable';
 
 class Port {
@@ -24,7 +26,7 @@ class Port {
 export class SelectEstheticiansPage {
  
   estheticianList: EstheticianList = undefined;
-  jsonData : any = [];
+  
 
   constructor(private appointement: AppointementService, private router: Router,
     private route: ActivatedRoute, private plt: Platform) {
@@ -41,7 +43,7 @@ esthdisp(){
 
       response => {
         this.estheticianList = this.convert(response);
-       
+      
       },
       error => {
         console.log(error);
@@ -50,25 +52,29 @@ esthdisp(){
 
   })
 
-  console.log(this.estheticianList);
-  this.jsonData=this.estheticianList;
-  console.log(this.jsonData);
+
 }
 
 FilterJSONData(ev :any){
-    this.esthdisp();
-    const val = ev.target.value;
-    const usersJson: any[] = Array.of(this.jsonData);
-      if(val && val.trim() != ''){
-            this.jsonData=usersJson.filter((item => {
+   
+let searchValue : string = ev.target.value;
 
-              return JSON.stringify(item.firstName.toLowerCase().includes(val.toLowerCase()) > -1);
-            //|| item.lastName.toLowerCase().indexOf(val.toLowerCase())>-1
-            })
-            )
-      }
-    this.jsonData = <any[]>JSON.parse(this.jsonData);
+    if(searchValue == null) {
+      return ;
+    }
+    console.log(searchValue);
+    if(searchValue == "") {
+ 
+      
+       return this.esthdisp();
+    }
 
+    let items = this.estheticianList.estheticianlist.filter((item => {
+      return (item.firstName.toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
+      }));
+
+   console.log(items);
+   this.estheticianList.estheticianlist = items;
 }
 
   convert(estheticianJSON: Any): EstheticianList {
