c2dc642ee62e9b50ed7f838f581b619c4861fdd8
Monsif EL AISSOUSSI <monsif.el-aissoussi-prestataire@laposte.fr>
2020-05-03 17:09:14 +0200

    add

--- back/src/main/java/com/elaissoussi/back/BackApplication.java
+++ back/src/main/java/com/elaissoussi/back/BackApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -16,7 +18,17 @@ public class BackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
-	
+
+	@RestController
+	class HelloController
+	{
+		@GetMapping
+		public String helloWord()
+		{
+			return "Hello Word";
+		}
+	}
+
 	@Bean
 	public WebMvcConfigurer configureCorsForWebMvc() {
 	  
